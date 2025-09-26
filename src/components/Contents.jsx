a741e2d4dd4174f5ff4ed39776520793a30fcb30
Farid <farid.ansari@parmarketing.agency>
2025-09-26 10:17:54 +0530

    update

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -278,7 +278,7 @@ const Contents = () => {
   };
 
   return (
-    <div className="contents" ref={contentsRef}>
+    <div className="contents hide" ref={contentsRef}>
       <ToastContainer />
       <div className="banner-desktop d-md-block d-none">
         <img src={bannerDesktop} alt="" />
