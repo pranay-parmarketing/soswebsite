e7501effe676fca4e736b50d86cd2836dd5dd073
Farid <farid.ansari@parmarketing.agency>
2025-09-26 12:03:13 +0530

    footer alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,17 +14,10 @@ const Footer = () => {
     <footer className="container-fluid background p-0">
       <div className="container p-2 pt-5">
         <div className="footer-top-flex">
-          <div className="footer-logo">
+          <div className="footer-logo mx-auto">
             <img src={logo} alt="" />
           </div>
-          <div>
-            <p className="red-text">#SoundsOfSilence</p>
-            <p className="red-text">#DebtFreeIndia</p>
-            <p className="red-text">#BreakTheSilence</p>
-          </div>
-        </div>
-        <div className="footer-bottom-flex mt-5">
-          <div>
+          <div className="d-lg-block d-none">
             <p>
               Join the #SoundsOfSilence initiative by SingleDebt. For too long,
               debt has stolen voices, sleep, and dignity—forcing millions of
@@ -38,40 +31,62 @@ const Footer = () => {
               despair.
             </p>
           </div>
-          <div className="footer-top-right">
-            <div className="text-end">
-              <button className="button">Visit forum</button>
+          <div className="mx-auto">
+            <div className="text-sm-end text-center">
+              <p className="red-text">#SoundsOfSilence</p>
+              <p className="red-text">#DebtFreeIndia</p>
+              <p className="red-text">#BreakTheSilence</p>
             </div>
-            <ul className="social-links">
-              <li>
-                <a href="#">
-                  <FaFacebook />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaInstagram />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaLinkedin />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaYoutube />
-                </a>
-              </li>
-            </ul>
+            <div className="footer-top-right mt-4">
+              <div className="text-sm-end text-center">
+                <button className="button">Visit forum</button>
+              </div>
+              <ul className="social-links">
+                <li>
+                  <a href="#">
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaInstagram />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaYoutube />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="mt-5">
+        <div className="d-lg-none mt-4">
+          <p>
+            Join the #SoundsOfSilence initiative by SingleDebt. For too long,
+            debt has stolen voices, sleep, and dignity—forcing millions of
+            middle class Indians to suffer in silence. This initiative is not
+            just awareness, it is care in action. A collective stand for hope,
+            healing, and support — where financial struggles meet compassion for
+            mental health. <br />
+            <br />
+            Together, we can shatter the stigma, give power back to silenced
+            voices, and build a future where no one drowns alone in quiet
+            despair.
+          </p>
+        </div>
+        <div className="footer-divider"></div>
+        <div className="mt-4">
           <p>
             Disclaimer: The statistics and information shared on this platform
             are derived from credible studies, mental health research, and
@@ -82,7 +97,7 @@ const Footer = () => {
         </div>
       </div>
       <div
-        className="px-2 py-3 mt-5 text-center"
+        className="px-2 py-3 mt-4 text-center"
         style={{ background: "#000" }}
       >
         <p className="text-uppercase">
